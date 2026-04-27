f8b7c6c3b4f4bcdc79b3898e23b423402a7a22ff
BenecioGonzales <beefy676@gmail.com>
2026-04-27 00:31:45 +0000

    more attempts

--- Benny_Coding.py
+++ Benny_Coding.py
@@ -1,6 +1,7 @@
 # Imports random number generator
 import random
 
+
 # Important variables for basics of code
 time_left = 30
 thr_pt = .3
@@ -8,16 +9,22 @@ two_pt = .5
 frthr  = .7
 off_rebound = .2
 ovr_vict = .5
-possesion = True
+team_pts = 0
+enemy_pts = 3
+
 
 
 # Three point calculator
 if time_left > 0:
     if random.random() < thr_pt:
-            + 3
-            time_left - 2
+            team_pts + 3
+            time_left - 10
     else:
-            + 0
-            time_left - 2
+            team_pts + 3
+            time_left - 10
 else:
-    break
+    if team_pts < enemy_pts:
+        loss
+    elif team_pts == enemy_pts:
+        if random.random() > ovr_vict:
+            
