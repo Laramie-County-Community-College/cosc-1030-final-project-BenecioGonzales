98125ab260fbb7a1870e2f26a60b8ec6bb7709f7
BenecioGonzales <beefy676@gmail.com>
2026-04-27 00:05:56 +0000

    Feeling out process

---
+++ Benny_Coding.py
@@ -0,0 +1,16 @@
+import random
+
+# Variable defining chances of making a shot and the time remaining
+time_left_in_sec = 30
+
+thr_pt_chance = .3
+two_pt_chance = .5
+ft_chance  = .7
+off_rebound = .2
+ovr_time_vict = .5
+
+# Three point calculator
+if random.random() < .3:
+    + 3
+else:
+    + 0
