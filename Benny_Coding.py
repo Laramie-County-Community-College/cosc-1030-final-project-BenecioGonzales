d94207ec6b1c88a3ec8f24a8fbee5b4ef0370a36
BenecioGonzales <beefy676@gmail.com>
2026-04-29 17:03:22 +0000

    cleaned up code slightly

--- Benny_Coding.py
+++ Benny_Coding.py
@@ -2,18 +2,11 @@
 import random    
 
 # Important variables for basics of code
-time_left = 30
 thr_pt_per = .32
 two_pt_per = .53
 fr_thr_per  = .68
 off_rebound = .21
 ovr_vict = .5
-team_pts = 0
-team_pts_total_thr = 0
-team_pts_total_two = 0
-enemy_pts = 3
-thr_pt_vict = 0
-two_pt_vict = 0
 
 # Defines the function
 def simulate(strategy):
@@ -25,21 +18,25 @@ def simulate(strategy):
         team_pts = 0
         enemy_pts = 3
         while time_left > 0:
-            if random.random() < thr_pt_per:
-                team_pts += 3
-                time_left -= random.randint(8, 12)
-            else:
-                time_left -= random.randint(8, 12)
+            
+            # Three point strategy
+            if strategy == "three"    
+                if random.random() < thr_pt_per:
+                    team_pts += 3
+                    time_left -= random.randint(8, 12)
+                else:
+                    time_left -= random.randint(8, 12)
 
-        # Score calculator
-        if team_pts > enemy_pts:
-            wins += 1
-        elif team_pts == enemy_pts:
-            if random.random() < ovr_vict:
+            # Two point strategy
+            
+            # Score calculator
+            if team_pts > enemy_pts:
                 wins += 1
-            else:
-                wins += 0
-        team_pts_total += team_pts
+            elif team_pts == enemy_pts:
+                if random.random() < ovr_vict:
+                    wins += 1
+            team_pts_total += team_pts
+    return wins, team_pts_total
 
 # Three point calculator
 for times in range(10000):
@@ -101,8 +98,8 @@ for times in range(10000):
             two_pt_vict += 0
     team_pts_total_two += team_pts
 
-thr_wins, thr_avg_pts = simulate("three")
-two_wins, two_avg_pts = simulate("two")
+wins_3pt, thr_total_pts = simulate("three")
+wins_2pt, two_total_pts = simulate("two")
 
-print(f"The percentage chance of winning only going for threes is {thr_pt_vict/10000 * 100:.1f}% and the average points scored is {team_pts_total_thr/10000:.2f}")
-print(f"The percentage chance of winning only going for twos is {two_pt_vict/10000 * 100:.1f}% and the average points scored is {team_pts_total_two/10000:.2f}")
+print(f"The percentage chance of winning only going for threes is {wins_3pt/10000 * 100:.1f}% and the average points scored is {thr_total_pts/10000:.2f}")
+print(f"The percentage chance of winning only going for twos is {wins_2pt/10000 * 100:.1f}% and the average points scored is {two_total_pts/10000:.2f}")
